ee104bb5ebe4efac3bcaada75cd49e28ca34312f
Caleb Lee <caleb.gwl@gmail.com>
2018-02-21 22:27:35 -0600

    Fix validation error

--- app/validations.js
+++ app/validations.js
@@ -2,7 +2,7 @@ export default function validateForm() {
   const fields = ['name', 'ingredients', 'directions', 'cookTime', 'servings'];
   for (let i = 0; i < fields.length; i++) {
     const fieldName = fields[i];
-    if (document.forms['newRecipeForm'][fields[i]].value === ') {
+    if (document.forms['newRecipeForm'][fields[i]].value === '') {
       const convertedText = fieldName.charAt(0).toUpperCase() + fieldName.slice(1);
       alert(convertedText + ' can not be empty');
           
